65c4c175ac8375da56e25d515528ad33cb989635
mathewmelvinng <54765658+mathewmelvinng@users.noreply.github.com>
2023-02-25 11:05:09 +1100

    created PlatfromMovement script

---
+++ MishMash/Assets/Scripts/Interfaces/IInputProvider.cs
@@ -0,0 +1,10 @@
+﻿using Enums;
+
+namespace Interfaces
+{
+    public interface IInputProvider
+    {
+        public float GetAxis(Axis axis);
+        public bool GetActionPressed(InputAction action);
+    }
+}
